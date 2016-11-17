490d973659c4b481dc11c4a31728080973f83afd
Chris Clifford and Joshua Rinard <chris@>
2016-11-17 10:14:42 -0800

    be refactor with until loop

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -2,24 +2,40 @@ class Fixnum
   define_method(:coin_combinations) do
     final_change = []
     cents_remaining = self
-    if cents_remaining % 25 == 0
-      quarters = cents_remaining / 25
-      final_change.push(quarters.to_s + " quarters")
-    elsif cents_remaining >= 25
-      remainder = cents_remaining % 25
-      quarters = (cents_remaining - remainder)/ 25
-      final_change.push(quarters.to_s + " quarters")
-      cents_remaining = remainder
-    if cents_remaining % 10 == 0
-      dimes = cents_remaining / 10
-      final_change.push(dimes.to_s + " dimes")
-    elsif cents_remaining >= 10
-      remainder = cents_remaining % 10
-      dimes = (cents_remaining - remainder)/ 10
-      final_change.push(dimes.to_s + " dimes")
-      cents_remaining = remainder
-    end
+    coin_array = ['quarters', 'dimes', 'nickle', 'pennies' ]
+    values_array = [25, 10, 5, 1]
+
+    counter = 0
+    until cents_remaining == 0
+      if cents_remaining >= values_array[counter]
+        remainder = cents_remaining % values_array[counter]
+        coin_number = (cents_remaining - remainder)/ values_array[counter]
+        final_change.push(coin_number.to_s + " " + coin_array[counter])
+        cents_remaining = remainder
+      end
+        counter += 1
     end
     final_change.join(", ")
   end
 end
+
+
+# counter = 0
+# until cents_remaining == 0
+#   if cents_remaining % 25 == 0
+#     quarters = cents_remaining / 25
+#     final_change.push(quarters.to_s + " quarters")
+#   elsif cents_remaining >= 25
+#     remainder = cents_remaining % 25
+#     quarters = (cents_remaining - remainder)/ 25
+#     final_change.push(quarters.to_s + " quarters")
+#     cents_remaining = remainder
+#   if cents_remaining % 10 == 0
+#     dimes = cents_remaining / 10
+#     final_change.push(dimes.to_s + " dimes")
+#   elsif cents_remaining >= 10
+#     remainder = cents_remaining % 10
+#     dimes = (cents_remaining - remainder)/ 10
+#     final_change.push(dimes.to_s + " dimes")
+#     cents_remaining = remainder
+#   end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -8,5 +8,11 @@ describe('Fixnum#coin_combinations') do
   it('if you give it 70 cents it returns 2 quarters and 2 dimes') do
   expect((70).coin_combinations()).to(eq("2 quarters, 2 dimes"))
   end
+  it('if you give it 65 cents it returns 2 quarters  1 dime and 1 nickle') do
+  expect((65).coin_combinations()).to(eq("2 quarters, 1 dimes, 1 nickle"))
+  end
+  it('if you give it 99 cents it returns 2 quarters, 1 dimes, 1 nickle, 4 pennies') do
+  expect((69).coin_combinations()).to(eq("2 quarters, 1 dimes, 1 nickle, 4 pennies"))
+  end
 
 end
