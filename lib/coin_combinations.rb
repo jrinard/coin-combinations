91818bd5241d6604e6da6634c85de4bb88044987
Chris Clifford and Joshua Rinard <chris@>
2016-11-17 09:42:23 -0800

    adds spec and code for dimes

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,9 +1,25 @@
 class Fixnum
   define_method(:coin_combinations) do
-    final_change = ""
-    if self % 25 == 0
-      quarters = self / 25
-      final_change = quarters.to_s + " quarters"
+    final_change = []
+    cents_remaining = self
+    if cents_remaining % 25 == 0
+      quarters = cents_remaining / 25
+      final_change.push(quarters.to_s + " quarters")
+    elsif cents_remaining >= 25
+      remainder = cents_remaining % 25
+      quarters = (cents_remaining - remainder)/ 25
+      final_change.push(quarters.to_s + " quarters")
+      cents_remaining = remainder
+    if cents_remaining % 10 == 0
+      dimes = cents_remaining / 10
+      final_change.push(dimes.to_s + " dimes")
+    elsif cents_remaining >= 10
+      remainder = cents_remaining % 10
+      dimes = (cents_remaining - remainder)/ 10
+      final_change.push(dimes.to_s + " dimes")
+      cents_remaining = remainder
     end
+    end
+    final_change.join(", ")
   end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -5,5 +5,8 @@ describe('Fixnum#coin_combinations') do
   it('if you give it a dollar it returns 4 quarters') do
   expect((100).coin_combinations()).to(eq("4 quarters"))
   end
-  
+  it('if you give it 70 cents it returns 2 quarters and 2 dimes') do
+  expect((70).coin_combinations()).to(eq("2 quarters, 2 dimes"))
+  end
+
 end
